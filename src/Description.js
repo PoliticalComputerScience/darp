38f8e782a8e86ba28c8fce05dd54d3686b10da11
Christopher Cooper <cooperc@ocf.berkeley.edu>
2019-03-21 10:26:06 -0700

    separate out map and description

---
+++ src/Description.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Description extends Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					Description!
+				</h1>
+				<p>
+					Lorem ipsum dolor imet...
+				</p>
+			</div>
+		);
+	}
+}
+
+export default Description;
