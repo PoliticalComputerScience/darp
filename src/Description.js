070864dd6bb6751726a4bc1089518c0b37642348
Christopher Cooper <christopher.c.cooper@gmail.com>
2019-04-06 17:39:12 -0700

    show more description underneath the map

--- src/Description.js
+++ src/Description.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 
+
+
 class Description extends Component {
 	render() {
+		if (this.props.country == null) {
+			return (
+				<h1>
+					Select a country to see more.
+				</h1>
+			);
+		}
+		if (!(this.props.country in this.props.data)) {
+			return (
+				<h1>
+					No data on this country.
+				</h1>
+			);
+		}
+		const data = this.props.data[this.props.country];
 		return (
 			<div>
 				<h1>
-					Description for country{this.props.country && (' with id ' + this.props.country)}!
+					{data.name}
 				</h1>
+				<h2>
+					<div style={{
+						'display': 'inline',
+						'font-weight': 'normal',
+						'padding-right': '5px',
+					}}>
+						Overall:
+					</div>
+					{data.overall}
+				</h2>
 				<p>
-					Lorem ipsum dolor imet...
+					{data.description}
 				</p>
 			</div>
 		);
